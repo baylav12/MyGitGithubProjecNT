9c87201826431bd8aee16de0d6229f93fd217e1c
Can <hamiitbaylav@gmail.com>
2023-04-26 22:00:43 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,10 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm projesidir");
 
+        // versiyon1
+        System.out.println("Java is easy ");
+        //versiyon2
+
 
 
 
